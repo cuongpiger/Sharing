42f7d7e650a976716fbc37f61c5177aae4c08cf0
Manh Cuong <cuongpigerr@gmail.com>
2022-01-05 13:55:54 +0700

    This is Manh Cuong's commit 🌻.

--- Clover/SignInFragment.java
+++ Clover/SignInFragment.java
@@ -1,18 +1,30 @@
-private void signIn() {
-    mSignIn.setClickable(false);
-    mCircle.setVisibility(View.VISIBLE);
+// file SignInFragment.java
 
-    DatabaseModel.signIn(email, password).addOnCompleteListener(new OnCompleteListener() {
-        @Override
-        public void onComplete(@NonNull Task task) {
-            if (task.isSuccessful()) {
-                DatabaseModel.getCurrentUser();
-                getActivity().finishAffinity();
-                getActivity().startActivity(new Intent(getActivity(), MainActivity.class));
-                Reuse.startActivity(getActivity());
-            } else {
-                errorDialog();
+import com.google.firebase.auth.FirebaseAuth; // import thư viện Firebase Authentication
+
+/*
+Phương thức dùng để đăng nhập sử dụng Firebase Authentication,
+phương thức này nằm trong phương thức setOnClickListener và
+được gọi khi người dùng nhấn vào button đăng nhập
+
+PARAMS:
+    email: là email của người dùng, lấy từ view.
+    password: là password của người dùng, lấy từ view.
+*/
+
+private void signIn(String email, String password) {
+    FirebaseAuth.getInstance()
+        .signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()) {
+                    /* nếu Firebase Authentication xác nhận người dùng này
+                    này có tồn tại và đăng nhập thành công, đi đến màn hình chính*/
+                } else {
+                    /*Thất bại do sai password, email không tồn tại trong Firebase
+                    Authentication => hiển thị Alert Dialog báo lỗi */
+                }
             }
-        }
     });
 }
