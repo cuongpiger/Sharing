484962b599b9fbc75a0ea65ff5bb4d1e9f9d9b95
Manh Cuong <cuongpigerr@gmail.com>
2022-01-07 22:49:41 +0700

    This is Manh Cuong's commit 🌻.

---
+++ SCL_DLL_Lab_02/Assignment_08/Assignment_08.java
@@ -0,0 +1,77 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+public class Assignment_08 {
+    private enum COUNTERS {
+        INVALID_RECORD_COUNT
+    }
+
+    public class LastFMConstants {
+        public static final int USER_ID = 0;
+        public static final int TRACK_ID = 1;
+        public static final int IS_SHARED = 2;
+        public static final int RADIO = 3;
+        public static final int IS_SKIPPED = 4;
+
+    }
+
+    public static class UniqueListenersMapper extends Mapper< Object , Text, IntWritable, IntWritable > {
+        IntWritable trackId = new IntWritable();
+        IntWritable userId = new IntWritable();
+        public void map(Object key, Text value, Mapper< Object , Text, IntWritable, IntWritable > .Context context)
+                throws IOException, InterruptedException {
+            String[] parts = value.toString().split("[|]");
+            trackId.set(Integer.parseInt(parts[LastFMConstants.TRACK_ID]));
+            userId.set(Integer.parseInt(parts[LastFMConstants.USER_ID]));
+            if (parts.length == 5) {
+                context.write(trackId, userId);
+            } else {
+                context.getCounter(COUNTERS.INVALID_RECORD_COUNT).increment(1L);
+            }
+        }
+    }
+
+    public static class UniqueListenersReducer extends Reducer< IntWritable , IntWritable, IntWritable, IntWritable> {
+
+        public void reduce(IntWritable trackId,Iterable< IntWritable > userIds,Reducer< IntWritable , IntWritable,
+                IntWritable,IntWritable>.Context context) throws IOException, InterruptedException {
+            Set< Integer > userIdSet = new HashSet< Integer >();
+            for (IntWritable userId : userIds) {
+                userIdSet.add(userId.get());
+            }
+            IntWritable size = new IntWritable(userIdSet.size());
+            context.write(trackId, size);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        if (args.length != 2) {
+            System.err.println("Usage: uniquelisteners < in >< out >");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "Unique listeners per track");
+        job.setJarByClass(Assignment_08.class);
+        job.setMapperClass(UniqueListenersMapper.class);
+        job.setReducerClass(UniqueListenersReducer.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        org.apache.hadoop.mapreduce.Counters counters = job.getCounters();
+        System.out.println("No. of Invalid Records :" + counters.findCounter(COUNTERS.INVALID_RECORD_COUNT).getValue());
+    }
+}
